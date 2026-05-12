eb6bea6f9d0d278de0ed29ab78a4cae284d3413d
mirco091 <fumiya116059@gmail.com>
2026-05-12 12:15:53 +0900

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+export default defineConfig({
+  base: "/mirco-musicgames/",
+  plugins: [react()],
+  resolve: { alias: { "@": path.resolve(__dirname, "src"), "@assets": path.resolve(__dirname, "src/assets") } },
+  build: { outDir: "dist" },
+});
